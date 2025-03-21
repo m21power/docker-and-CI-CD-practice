a997899947357bfe9996b757e67ddea1abe3e34c
mesay <mesaylema21@gmail.com>
2025-03-21 19:25:47 +0300

    fix: fix the error

--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -5,7 +5,7 @@ def test_fizzbuzz():
     assert fizzbuzz(15) == "FizzBuzz"  # Divisible by 3 and 5
     assert fizzbuzz(9) == "Fizz"       # Divisible by 3
     #let's change here and make it fail
-    assert fizzbuzz(10) == "Fizz"      # Divisible by 5
+    assert fizzbuzz(10) == "Buzz"      # Divisible by 5
     assert fizzbuzz(7) == 7            # Not divisible by 3 or 5
 
 if __name__ == "__main__":
