674c2b5217c3af1ee8d5406601579d0e3ea5e1df
mesay <mesaylema21@gmail.com>
2025-03-21 19:24:32 +0300

    let's make it fail

--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -4,7 +4,8 @@ from fizzbuzz import fizzbuzz
 def test_fizzbuzz():
     assert fizzbuzz(15) == "FizzBuzz"  # Divisible by 3 and 5
     assert fizzbuzz(9) == "Fizz"       # Divisible by 3
-    assert fizzbuzz(10) == "Buzz"      # Divisible by 5
+    #let's change here and make it fail
+    assert fizzbuzz(10) == "Fizz"      # Divisible by 5
     assert fizzbuzz(7) == 7            # Not divisible by 3 or 5
 
 if __name__ == "__main__":
